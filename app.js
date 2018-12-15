22c2ad95a11198585ffff14493e04775d300559d
petrlr14 <pedrodeveloper14@gmail.com>
2018-12-15 14:05:45 -0600

    a punto para prueba

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const Hapi=require("hapi");
 const UserRoutes=require("./routes/userRoutes");
 
 const server=Hapi.Server({
-    port:3000,
-    host:'localhost',
+    port:+process.env.PORT,
+    host:'0.0.0.0',
     app:{}
 });
 
@@ -14,7 +14,6 @@ const start=async()=>{
     try{
         await server.register(UserRoutes);
         await server.start();
-        console.log(`Server is running at: ${server.info.uri}`);
         require("./database/database");
     }catch(err){
         console.log("Error staring Hapi server", err);
